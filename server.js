39800503f1cda1c48eeac52409d4997e01edf383
mannu <mannu.singh@habilelabs.io>
2020-07-15 10:23:35 +0530

    added running node server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+var path    = require("path");
+
+app.get('/',function(req,res){  
+    res.sendFile(path.join(__dirname,'public/index.html')); 
+ });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
